1fba3afb6b5aa940605f70518401a0f2d035bca3
juanortiz93 <jcarlosortizalvarez@gmail.com>
2024-02-12 18:05:03 -0500

    agregar clases, objetos y arrays

--- index.js
+++ index.js
@@ -1,34 +1,103 @@
-let producto,valor,tarjeta,descuento;//Declaración de variables
+let producto,tarjeta;//Declaración de variables
 
-function elegirMedioDePago(){// funcion para mostrar el menu para elegir el medio de pago a usar para cotizar el descuento
-      let opcionTarjeta = prompt("1. Tarjeta Falabella\n 2. Tarjeta Tuya\n 3. Otro medio de pago\n Eliga el medio de pago: ");
-    switch(parseInt(opcionTarjeta)){
-        case 1:
-            tarjeta="Tarjeta Falabella";
-            descuento= 20;
-            break;
+const productos = [
+    {
+        nombre: "TV Sony 46 pulgadas",
+        valor: 3500000,
+        imagen: "./img/tv50pulgadas.jpg"
 
-        case 2:
-            tarjeta="Tarjeta Tuya";
-            descuento= 10;
-            break;
+    },
+    {
+        nombre: "Celular Samsung S24 Ultra",
+        valor: 5500000,
+        imagen: "./img/samsungS24.jpeg"
 
-        case 3:
-            tarjeta="Otro medio de pago";
-            descuento= 0;
-            break;
-                
+    },
+    {
+        nombre: "Computador Acer 15 pulgadas",
+        valor: 4600000,
+        imagen: "./img/acer15.jpeg"
+
+    },
+    {
+        nombre: "Audifonos Bose",
+        valor: 2400000,
+        imagen: "./img/boseHeadset.jpeg"
+
+    },
+
+];
+
+const medioDePago = [
+    {
+        tarjetaNombre: "Tarjeta Falabella",
+        descuento: 20
+    },
+    {
+        tarjetaNombre: "Tarjeta Tuya",
+        descuento: 30
+    },
+    {
+        tarjetaNombre: "Tarjeta Davivienda",
+        descuento: 25
+    },
+    {
+        tarjetaNombre: "Tarjeta Scotiabank",
+        descuento: 10
+    },
+
+]
+class Producto{
+    constructor(nombreProducto,valorProducto,imagenProducto){
+        this.nombreProducto = nombreProducto;
+        this.valorProducto = valorProducto;
+        this.imagenProducto = imagenProducto;
+    }
+
+}
+
+class Tarjeta{
+    constructor(nombreTarjeta,descuentoTarjeta){
+        this.nombreTarjeta = nombreTarjeta;
+        this.descuentoTarjeta = descuentoTarjeta;
     }
 }
 
+
+
+
+function elegirMedioDePago(){// funcion para mostrar el menu para elegir el medio de pago a usar para cotizar el descuento
+        let mensajeMediodePago= "por favor eliga uno de nuestros medios pago:\n ";
+        medioDePago.forEach((medioPago,indice)=>{    
+        mensajeMediodePago +=  (indice+1)+". "+medioPago.tarjetaNombre+"\n";    
+        });
+      let opcionTarjeta = prompt(mensajeMediodePago);
+       if (opcionTarjeta >= 0 && opcionTarjeta <= medioDePago.length) {
+            tarjeta = new Tarjeta(medioDePago[opcionTarjeta-1].tarjetaNombre,medioDePago[opcionTarjeta-1].descuento); 
+       } else if ( opcionTarjeta > medioDePago.length){
+        console.warn("Opción Invalida, por favor ingrese un numero valido:")
+       } else {
+        console.warn("Opción Invalida, por favor ingrese un numero valido:")
+       }
+            
+         
+}
+
+function aplicarIVA(valor){
+    let iva = valor*0.19;
+    return iva;
+}
+
 function calcularValorDescuento(){// funcion para calcular el valor del producto con base en el descuento seleccionado
-    let valorConDescuento= parseInt(valor-(valor*(descuento/100)));
-    console.log("Checkout\n Producto: "+producto+"\n Medio de pago: "+tarjeta+"\n Valor: $"+valor+"\n Descuento: "+descuento+"%\n Valor Total: $"+valorConDescuento);
+    let valorConDescuento= parseInt(producto.valorProducto-(producto.valorProducto*(tarjeta.descuentoTarjeta/100)));
+    let subtotal = valorConDescuento-aplicarIVA(valorConDescuento);
+    console.log("Checkout\n Producto: "+producto.nombreProducto+"\n Medio de pago: "+tarjeta.nombreTarjeta+"\n Valor: $"+producto.valorProducto+"\n Descuento: "+tarjeta.descuentoTarjeta+"%\n Subtotal: $"+subtotal+"\n IVA: $"+aplicarIVA(valorConDescuento)+"\n Valor Total: $"+valorConDescuento);
 }
 
 function CalcularDescuentoCupon(descuentoCupon,cadenaCupon){//funcion para calcular el nuevo valor de acuerdo al cupon de descuento aplicado
-    valorConDescuento = parseInt(valor-(valor*((descuento+descuentoCupon)/100)));
-    console.log("Checkout\n Producto: "+producto+"\n Medio de pago: "+tarjeta+"\n Valor: $"+valor+"\n Cupon: "+cadenaCupon+"\n Descuento: "+parseInt(descuento+descuentoCupon)+"%\n Valor Total: $"+valorConDescuento);
+    valorConDescuento = parseInt(producto.valorProducto-(producto.valorProducto*((tarjeta.descuentoTarjeta+descuentoCupon)/100)));
+    let subtotal = valorConDescuento-aplicarIVA(valorConDescuento);
+    console.log("Checkout\n Producto: "+producto.nombreProducto+"\n Medio de pago: "+tarjeta.nombreTarjeta+"\n Valor: $"+producto.valorProducto+"\n Cupon: "+cadenaCupon+"\n Descuento: "+parseInt(tarjeta.descuentoTarjeta+descuentoCupon)+"%\n Subtotal: $"+subtotal+"\n IVA: $"+aplicarIVA(valorConDescuento)+"\n Valor Total: $"+valorConDescuento);
 }
 
 function aplicarCupon(){// funcion para aplicar el descuento de acuerdo al cupon seleccionado si el usuario dice SI o NO
@@ -53,7 +122,7 @@ function aplicarCupon(){// funcion para aplicar el descuento de acuerdo al cupon
             calcularValorDescuento();
         }
     }
-    else  {2
+    else  {
         calcularValorDescuento();
         alert("Gracias, vuelva pronto");
 
@@ -64,48 +133,25 @@ function aplicarCupon(){// funcion para aplicar el descuento de acuerdo al cupon
 /*Menu principal que contiene los productos disponibles para ser seleccionados y va llamando en cada opción las distintas funciones
 para seleccionar el medio de pago aplicar el descuento y aplicar el cupon de descuento y mostrar el checkout al usuario
 */
+let mensajeMenu= "Bienvenido, por favor eliga uno de nuestros productos\n Ingrese la opción que desea: \n";
+productos.forEach((product,indice)=>{    
+    mensajeMenu +=  (indice+1)+". "+product.nombre+"\n";    
+});
 
-    let opcion = prompt("Bienvenido, por favor eliga uno de nuestros productos\n 1. TV Sony 46 pulgadas\n 2. Celular Samsung S24\n 3. Computador Acer 15 pulgadas\n 4. Audifonos Bose\n 5. Salir\n Ingrese la opción que desea: ");
-    switch(parseInt(opcion)){
-        case 1:
-            producto="TV Sony 46 pulgadas";
-            valor= 3500000;
-            console.log("Producto Seleccionado: "+producto+". Valor: $"+valor);
-            elegirMedioDePago();
-            calcularValorDescuento();
-            aplicarCupon();
-            break;
+    let opcion = prompt(mensajeMenu+" 5. Salir\n");
+     producto = new Producto(productos[opcion-1].nombre,parseInt(productos[opcion-1].valor,productos[opcion-1].imagen))
+     console.log("Producto Seleccionado: "+producto.nombreProducto+". Valor: $"+producto.valorProducto);
+     elegirMedioDePago();
+     calcularValorDescuento();
+    aplicarCupon();
 
-        case 2:
-            producto="Celular Samsung S24";
-            valor= 5500000;
-            console.log("Producto Seleccionado: "+producto+". Valor: $"+valor);
-            elegirMedioDePago();
-            calcularValorDescuento();
-            aplicarCupon();
-            break;
 
-        case 3:
-            producto="Computador Acer 15 pulgadas";
-            valor= 4750000;
-            console.log("Producto Seleccionado: "+producto+". Valor: $"+valor);
-            elegirMedioDePago();
-            calcularValorDescuento();
-            aplicarCupon();
-            break;
-        
-        case 4:
-            producto="Audifonos Bose";
-            valor= 2200000;
-            console.log("Producto Seleccionado: "+producto+". Valor: $"+valor);
-            elegirMedioDePago();
-            calcularValorDescuento();
-            aplicarCupon();
-            break;
 
-        case 5:
-            alert("Gracias, vuelva pronto")
-            break;
-    }
+
+
+    
+    
+
+    
 
 
